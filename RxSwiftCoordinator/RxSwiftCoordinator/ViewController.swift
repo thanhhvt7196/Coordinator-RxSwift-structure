e95a326f54453e3952655d171dc06c89f14d3a54
mlh-thanhhvt <thanhhvt@lifetimetech.vn>
2020-09-11 09:24:54 +0700

    initial project

---
+++ RxSwiftCoordinator/RxSwiftCoordinator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxSwiftCoordinator
+//
+//  Created by thanh tien on 9/11/20.
+//  Copyright © 2020 thanh tien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
